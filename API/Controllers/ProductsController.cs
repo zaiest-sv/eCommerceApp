22054de41055ece369e39d0fcdc7e7318c495f26
Serhii Zaets <>
2023-07-01 11:55:24 +0300

    API initialization

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    [HttpGet]
+    public string GetProducts()
+    {
+        return "GetProducts";
+    }
+    
+    [HttpGet("{id}")]
+    public string GetProductById(int id)
+    {
+        return "Get Product By Id";
+    }
+}
+
