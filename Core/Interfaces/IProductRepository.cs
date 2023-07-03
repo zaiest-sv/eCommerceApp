b19f5e26bba35e685e87b6aba6b73cbeb2c39a79
Serhii Zaets <>
2023-07-03 14:23:57 +0300

    Refactor Initial Migration and Update Data Seeding
    Updated the initial database migration to more accurately reflect the domain models and added new seed data for product types and brands.
    Also wire up product repository to the API controller to allow for easier data access and manipulation.
    
    Starting with the API layer, the products controller now makes use of the product repository for data access, instead of going directly to the DbContext.
    In the Infrastructure project, the initial migration has been modified to allow for handling of more complex domain models, with the addition of `ProductType` and `ProductBrand` related details.
    Corresponding seed data has been added, including products.json, types.json, and brands.json files.
    The EntityFrameworkCore's `OnModelCreating` method is also updated to apply configurations from the current assembly, which includes brand and type entities mappings. For data seeding, the store context now uses an updated SeedData class that reads from the brand new Json files.
    These changes will set the groundwork for upcoming feature developments and improve the reliability of database migrations and data seeding.

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities;
+
+namespace Core.Interfaces;
+
+public interface IProductRepository
+{
+    Task<Product> GetProductByIdAsync(int id);
+    Task<IReadOnlyList<Product>> GetProductAsync();
+    Task<IReadOnlyList<ProductBrand>> GetProductBrandAsync();
+    Task<IReadOnlyList<ProductType>> GetProductTypeAsync();
+}
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,4 +1,5 @@
-﻿using Core.Entities;
+﻿using System.Reflection;
+using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
@@ -10,4 +11,12 @@ public class StoreContext : DbContext
     }
     
     public DbSet<Product> Products { get; set; }
+    public DbSet<ProductBrand> ProductBrand { get; set; }
+    public DbSet<ProductType> ProductType { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
 }
