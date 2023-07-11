2916f1ed88cfee8312ea24e26d9d06301f09003d
Serhii Zaets <>
2023-07-11 12:33:31 +0300

    Add error handling and controller refactoring
    Added a middleware for global exception handling and custom error handling logic. Created a new BuggyController.cs for testing different types of errors such as NotFound, BadRequest, and ServerError. Replaced inline exception handling in ProductsController.cs with a centralized approach. Refactored Controllers to inherit from a BaseApiController. Enhanced the .gitignore file with additional patterns for files to be ignored from the repo. Updated 'Program.cs', implementing the middleware's use and refactoring the service adding section. Adjusted Swagger configuration to allow proper work with error handling scenarios.

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using API.Dtos;
+using API.Errors;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
@@ -7,9 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-public class ProductsController : ControllerBase
+public class ProductsController : BaseApiController
 {
     private readonly IGenericRepository<Product> _productRepository;
     private readonly IGenericRepository<ProductBrand> _productBrandRepository;
@@ -36,11 +35,18 @@ public class ProductsController : ControllerBase
     }
     
     [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
     {
         var specification = new ProductWithTypesAndBrandsSpecification(id);
         var product = await _productRepository.GetEntityWithSpecification(specification);
 
+        if (product == null)
+        {
+            return NotFound(new ApiResponse(404));
+        }
+        
         return _mapper.Map<Product, ProductToReturnDto>(product);
     }
     
