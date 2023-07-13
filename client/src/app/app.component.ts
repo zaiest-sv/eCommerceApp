4fbe9aeb1830d19c8c88c50794fc10f26ad40c50
Serhii Zaets <>
2023-07-13 13:32:51 +0300

    Add pagination model and update App component
    Implement a Pagination<T> interface for the convenient pagination of various datatypes. The reference model is created in pagination.ts with properties like pageIndex, pageSize, count, and data. The App component design is drastically simplified, replacing the lengthy boilerplate with "<app-nav-bar>" and "<router-outlet>" in app.component.html, indicating a transition to individual structural components. Replace the dummy styling with necessary ones, clearing the clutter. This improves the overall structure and readability of the application code.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Product} from "./models/product";
+import {Pagination} from "./models/pagination";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+
+  products: Product[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products').subscribe({
+      next: response => this.products = response.data,
+      error: err => console.log('err ', err),
+      complete: () => {
+        console.log('request completed');
+      }
+    });
+  }
 }
